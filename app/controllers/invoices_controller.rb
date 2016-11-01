8d96c3ec5767dadf9317cd66c99faa201116174a
andrade94 <eduardo.mtz9@gmail.com>
2016-11-01 17:00:49 -0600

    final

--- app/controllers/invoices_controller.rb
+++ app/controllers/invoices_controller.rb
@@ -1,4 +1,3 @@
-require 'open-uri'
 class InvoicesController < ApplicationController
 	def viewInvoices
 	end
@@ -6,25 +5,24 @@ class InvoicesController < ApplicationController
 		# Parse the URI and retrieve it to a temporary file
 		xml_file = File.read("F0000000047.xml")
 		doc = Nokogiri::XML.parse(xml_file)
-		root = doc.root
+		@root = doc.root
 		receptor = doc.xpath('//cfdi:Receptor')
 		emisor = doc.xpath('//cfdi:Emisor')
 		total = doc.xpath('//cfdi:Comprobante')
 		impuestos = doc.xpath('//cfdi:Traslado')
 
-		nombreEmisor = emisor[0]["nombre"]
-		rfcEmisor = emisor[0]["rfc"]
-		nombreReceptor = receptor[0]["nombre"]
-		rfcReceptor = receptor[0]["rfc"]
-		subtotal = total[0]["subTotal"]
-		iva = impuestos[0]["importe"]
-		pagoTotal = total[0]["total"]
+		@nombreEmisor = emisor[0]["nombre"]
+		@rfcEmisor = emisor[0]["rfc"]
+		@nombreReceptor = receptor[0]["nombre"]
+		@rfcReceptor = receptor[0]["rfc"]
+		@subtotal = total[0]["subTotal"]
+		@iva = impuestos[0]["importe"]
+		@pagoTotal = total[0]["total"]
 		#html output
-		html =  "Emisor: " + nombreEmisor + " RFC Emisor : " + rfcEmisor + "\n" +
-				"Receptor: " + nombreReceptor + " RFC Receptor : " + rfcReceptor + "\n" +
-				"Subtotal: " + subtotal + " IVA: " + iva + " Total: " + pagoTotal
+		#html =  "Emisor: " + nombreEmisor + " RFC Emisor : " + rfcEmisor + "\n" +
+		#		"Receptor: " + nombreReceptor + " RFC Receptor : " + rfcReceptor + "\n" +
+		#		"Subtotal: " + subtotal + " IVA: " + iva + " Total: " + pagoTotal
 
 		#extract the title from the articles
-		render :text => html,:content_type => "text/plain"
 	end
 end
