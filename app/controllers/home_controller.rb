a578fc377674549a4f866c1d8b185849afa05484
MeritWayler <eugeniomartinez92@gmail.com>
2016-11-09 20:54:31 -0600

    login and sign up works appropriately

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,9 @@
 class HomeController < ApplicationController
   skip_before_action :authenticate_user!
-
-  def index
-  	@user = User.new
-  end
+  
+  # def index
+  # 	user = User.new
+  # end
 end
+
+#  form_for(User, url: registration_path(@user)) do |f| %>
--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -1,6 +1,15 @@
 class Users::SessionsController < Devise::SessionsController
 # before_action :configure_sign_in_params, only: [:create]
 
+  def create
+    super
+  end
+
+  protected
+
+  def after_sign_in_path_for(resource)
+    profile_path
+  end 
   # GET /resource/sign_in
   # def new
   #   super
